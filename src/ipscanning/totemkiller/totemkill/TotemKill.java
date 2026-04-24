fa8b5514c9017ce7ff5da4c47a54417dde79a3d5
ImIpscanning <mavchin8@gmail.com>
2026-04-24 02:01:14 +0200

    Update TotemKill.java

--- src/ipscanning/totemkiller/totemkill/TotemKill.java
+++ src/ipscanning/totemkiller/totemkill/TotemKill.java
@@ -3,6 +3,7 @@ package me.ImIpscanning.totemkill;
 import org.bukkit.Bukkit;
 import org.bukkit.EntityEffect;
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,8 +12,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TotemKill extends JavaPlugin implements Listener {
 
+    private FileConfiguration config;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig(); 
+        config = getConfig();
+
         Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info("TotemKill activado");
     }
@@ -24,10 +30,30 @@ public class TotemKill extends JavaPlugin implements Listener {
 
         if (killer == null) return;
 
-        // 🔥 Animación REAL de tótem
-        killer.playEffect(EntityEffect.TOTEM_RESURRECT);
+    
+        if (config.getBoolean("totem-animation", true)) {
+            killer.playEffect(EntityEffect.TOTEM_RESURRECT);
+        }
 
-        // 🔊 Sonido del tótem
-        killer.playSound(killer.getLocation(), Sound.ITEM_TOTEM_USE, 1f, 1f);
+ 
+        if (config.getBoolean("sound.enabled", true)) {
+            String soundName = config.getString("sound.type", "ITEM_TOTEM_USE");
+            float volume = (float) config.getDouble("sound.volume", 1.0);
+            float pitch = (float) config.getDouble("sound.pitch", 1.0);
+
+            try {
+                Sound sound = Sound.valueOf(soundName);
+                killer.playSound(killer.getLocation(), sound, volume, pitch);
+            } catch (IllegalArgumentException ex) {
+                getLogger().warning("Sonido inválido en config.yml: " + soundName);
+            }
+        }
+
+
+        if (config.getBoolean("message.enabled", false)) {
+            String msg = config.getString("message.text", "&aHas matado a %player%");
+            msg = msg.replace("%player%", muerto.getName());
+            killer.sendMessage(msg.replace("&", "§"));
+        }
     }
 }
